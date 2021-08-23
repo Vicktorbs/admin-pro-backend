30be302123193d6e5ba0aa0d6b70f90716fe20f8
Victor Bolaños Solís <viktorb.132@gmail.com>
2021-08-23 13:33:50 -0500

    Update and delete medics

--- controllers/medics.js
+++ controllers/medics.js
@@ -34,17 +34,66 @@ const createMedic = async(req, res) => {
     }
 
 }
-const updateMedic = (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'updateMedic'
-    })
+const updateMedic = async(req, res) => {
+    const id = req.params.id;
+    const uid = req.uid;
+    try {
+        const medic = await Medic.findById(id);
+
+        if (!medic) {
+            return res.status(404).json({
+                ok: true,
+                msg: 'Medic not found'
+            })
+        }
+
+        const medicChanges = {
+            ...req.body,
+            user: uid
+        }
+
+        const medicUpdated = await Medic.findByIdAndUpdate(id, medicChanges, { new: true });
+
+        res.json({
+            ok: true,
+            medic: medicUpdated
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Medic no updated'
+        })
+    }
 }
-const deleteMedic = (req, res) => {
-    res.json({
-        ok: true,
-        msg: 'deleteMedic'
-    })
+
+const deleteMedic = async(req, res) => {
+    const id = req.params.id;
+    try {
+        const medic = await Medic.findById(id);
+
+        if (!medic) {
+            return res.status(404).json({
+                ok: true,
+                msg: 'Medic not found'
+            })
+        }
+
+        await Medic.findByIdAndDelete(id);
+
+        res.json({
+            ok: true,
+            msg: 'Medic deleted'
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Medic no deleted'
+        })
+    }
 }
 
 module.exports = {
--- routes/medics.js
+++ routes/medics.js
@@ -17,9 +17,14 @@ router.post('/',
     createMedic
 );
 router.put('/:id', 
-    [], 
+    [
+        validateJWT,
+        check('name', 'Name is required').not().isEmpty(),
+        check('hospital', 'Hospital ID is required').isMongoId(),
+        validarCampos
+    ], 
     updateMedic
  );
-router.delete('/:id', deleteMedic);
+router.delete('/:id', validateJWT, deleteMedic);
 
 module.exports = router;
